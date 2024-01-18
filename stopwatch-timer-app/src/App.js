9f4c8682b8172908a5a1506cad9c11413fe7d208
kumakibear <128429317+kumakibear@users.noreply.github.com>
2024-01-18 10:17:02 +0000

    mensaje

--- stopwatch-timer-app/src/App.js
+++ stopwatch-timer-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Timer from './components/Timer';
-
 function App() {
   return (
       <div className="App">
             <Timer/>
+            
                 </div>
                   );
                   }
